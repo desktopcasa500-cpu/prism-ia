3601e7172b4c53b03bb8ddf9e04959e3d6da0a44
desktopcasa500-cpu <desktopcasa500@gmail.com>
2026-09-04 11:11:15 -0300

    Delete frontend directory

--- frontend/components/PrismCodexIntro.jsx
+++
@@ -1,5 +0,0 @@
-import PrismCodexIntroBrutalist, { INTRO_KEY } from './PrismCodexIntroBrutalist.jsx';
-import './prism-codex-intro-brutalist.css';
-
-export { INTRO_KEY };
-export default PrismCodexIntroBrutalist;

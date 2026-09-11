ecfc73c4c5c5f5a0cbfd54e74da6e165fb25b9ed
desktopcasa500-cpu <desktopcasa500@gmail.com>
2026-09-11 08:24:11 -0300

    Delete frontend directory

--- frontend/components/TaffPresentation.jsx
+++
@@ -1,9 +0,0 @@
-import PrismCodexIntroBrutalist from './PrismCodexIntroBrutalist.jsx';
-
-export default function TaffPresentation() {
-  const close = () => {
-    window.dispatchEvent(new KeyboardEvent('keydown', { key: 'Escape' }));
-  };
-
-  return <PrismCodexIntroBrutalist onComplete={close} />;
-}

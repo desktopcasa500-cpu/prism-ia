f4dddadcf73668ef7d8753b4afe870afe396b2d1
desktopcasa500-cpu <desktopcasa500@gmail.com>
2026-09-24 09:13:12 -0300

    chore: remove unused diffEngine.js

--- backend/src/services/diffEngine.js
+++
@@ -1,8 +0,0 @@
-export function createDiff(before='', after='') {
-  return {
-    removed: before ? before.split('\n').length : 0,
-    added: after ? after.split('\n').length : 0,
-    before,
-    after
-  };
-}
